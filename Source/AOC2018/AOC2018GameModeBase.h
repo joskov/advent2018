e0ff8af9af595f83c748e2fb943c17737a0a1ebe
Vasil Joskov <vjoskov@gmail.com>
2018-12-01 11:42:04 +0100

    Init.

---
+++ Source/AOC2018/AOC2018GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AOC2018GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AOC2018_API AAOC2018GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
